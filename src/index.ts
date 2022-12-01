e5cfa571841917996e81b901c3bbe74828246e73
Pixselve <38262536+Pixselve@users.noreply.github.com>
2022-12-01 22:25:40 +0100

    feat: remove lines

--- src/index.ts
+++ src/index.ts
@@ -284,7 +284,7 @@ function parseTopDownInstructions(data: any): string {
     "\n# Stages\n" +
     Array.from(stageOf.values())
       .map((stage) => stage.toMarkdown())
-      .join("\n---\n")
+      .join("\n")
   );
 }
 
