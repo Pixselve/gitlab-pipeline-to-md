621590abfc2ac322f860fb10c01c69797b3f9806
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-01-19 16:01:29 +0100

    refactor: use list

--- src/utils/mermaid.ts
+++ src/utils/mermaid.ts
@@ -1,23 +1,23 @@
-import { Stage } from "../index";
+import {Stage} from "../index";
 
 export function getMermaid(stages: Stage[]): string {
-  return `\`\`\`mermaid
-flowchart LR
-${stages.map((stage) => stage.toMermaid()).join("\n")}
-${getMermaidArrows(stages)}
-\`\`\``;
+    let result = [];
+    result.push("\`\`\`mermaid");
+    result.push("flowchart LR");
+    result.push(stages.map((stage) => stage.toMermaid()));
+    result.push(getMermaidArrows(stages));
+    result.push("\`\`\`");
+    return result.join("\n");
 }
 
 function getMermaidArrows(stages: Stage[]): string {
-  let arrows = "";
-  for (let i = 0; i < stages.length - 1; i++) {
-    const stage = stages[i];
-    const nextStage = stages[i + 1];
-    arrows += `${stage.name.replaceAll(
-      " ",
-      "_"
-    )}_STAGE --> ${nextStage.name.replaceAll(" ", "_")}_STAGE
-`;
-  }
-  return arrows;
+    let result = [];
+    for (let i = 0; i < stages.length - 1; i++) {
+        const stage = stages[i];
+        const nextStage = stages[i + 1];
+        const stageName = stage.name.replaceAll(" ", "_");
+        const nextStageName = nextStage.name.replaceAll(" ", "_");
+        result.push(`${stageName} --> ${nextStageName}`);
+    }
+    return result.join("\n");
 }
